d49c2c5c02c2655f0de26818807dace01f50e5f0
Alex Rundle <alexrundle01@gmail.com>
2023-03-16 15:42:48 +0000

    Started work on Gastner-Newman algorithm
    Added a large part of the Gastner-Newman algorithm

--- cartogram.py
+++ cartogram.py
@@ -1,3 +1,6 @@
+import pyfftw
+import pyfftw.interfaces.numpy_fft as fftw
+
 import border_util
 
 import geopandas as gpd
@@ -15,6 +18,23 @@ def paired_distance(X, Y):
 
 class Cartogram:
     def __init__(self, gdf, value_field, id_field=None, geometry_field='geometry'):
+
+        # Initialise Gastner-Newman variables
+        self.xsize = None
+        self.ysize = None
+        self.rhot = None
+        self.fftrho = None
+        self.fftexpt = None
+        self.expky = None
+        self.grid_points_x = None
+        self.grid_points_y = None
+        self.errorp = None
+        self.drp = None
+        self.inith = 0.001
+        self.target_error = 0.01
+        self.max_ratio = 4.0
+        self.expected_time = 100000000.0
+
         self.gdf = gdf
         self.value_field = value_field
         self.geo_field = geometry_field
@@ -246,9 +266,323 @@ class Cartogram:
 
             fishnet['density'] = densities
 
-            return fishnet
+            return fishnet, n_cells_x, n_cells_y
 
-    # TODO: Implement rest of Gastner-Newman diffusion algorithm
+
+        def create_grid_of_points():
+            self.grid_points_x = np.zeros((self.xsize + 1) * (self.ysize + 1))
+            self.grid_points_y = np.zeros((self.xsize + 1) * (self.ysize + 1))
+
+            i = 0
+
+            for iy in range(0, self.ysize+1):
+                for ix in range(0, self.xsize+1):
+                    self.grid_points_x[i] = ix
+                    self.grid_points_y[i] = iy
+                    i += 1
+
+        def compute_initial_density(density_grid):
+            densities = density_grid['density'].values
+
+            # Convert density grid to column-major form
+            rho = np.array(densities).reshape((self.xsize, self.ysize), order='F')
+
+            # self.fftrho = pyfftw.empty_aligned((self.xsize, self.ysize//2+1), dtype='float64')
+
+            fft_plan = pyfftw.builders.rfftn(rho, s=(self.xsize, self.ysize), axes=(0, 1), planner_effort='FFTW_ESTIMATE')
+            self.fftrho = fft_plan(rho)
+
+
+        def density_snapshot(t, s):
+            ix, iy = 0, 0
+            kx, ky = 0, 0
+            expkx = 0
+
+            print(self.xsize, self.ysize)
+
+            # Calculate expky array to save time in next part
+            for iy in range(0, self.ysize):
+                ky = np.pi * iy / self.ysize
+                self.expky[iy] = np.exp(-ky * ky * t)
+
+            for ix in range(0, self.xsize):
+                kx = np.pi * ix / self.xsize
+                expkx = np.exp(-kx * kx * t)
+
+                # TODO: fftrho is (100, 51) instead of (100, 100)
+                for iy in range (0, self.ysize):
+                    self.fftexpt[ix][iy] = expkx * self.expky[iy] * self.fftrho[ix][iy]
+                    self.rhot[s][ix][iy] = self.fftexpt[ix][iy]
+
+            # Perform back-transform
+            fft_plan = pyfftw.builders.irfftn(self.rhot[s], s=(self.xsize, self.ysize), axes=(0, 1), planner_effort='FFTW_ESTIMATE')
+            self.fftrho = fft_plan(self.rhot[s])
+
+
+        def velocity(rx, ry, s):
+            ix = int(rx)
+
+            if ix < 0:
+                ix = 0
+            elif ix >= self.xsize:
+                ix = self.xsize - 1
+
+            ixm1 = ix - 1
+            if ixm1 < 0:
+                ixm1 = 0
+
+            ixp1 = ix + 1
+            if ixp1 >= self.xsize:
+                ixp1 = self.xsize - 1
+
+            iy = int(ry)
+            if iy < 0:
+                iy = 0
+            elif iy >= self.ysize:
+                iy = self.ysize - 1
+
+            iym1 = iy - 1
+            if iym1 < 0:
+                iym1 = 0
+
+            iyp1 = iy + 1
+            if iyp1 >= self.ysize:
+                iyp1 = self.ysize - 1
+
+            # Calculate densities at nine surrounding grid points
+            rho00 = self.rhot[s][ixm1][iym1]
+            rho10 = self.rhot[s][ix][iym1]
+            rho20 = self.rhot[s][ixp1][iym1]
+            rho01 = self.rhot[s][ixm1][iy]
+            rho11 = self.rhot[s][ix][iy]
+            rho21 = self.rhot[s][ixp1][iy]
+            rho02 = self.rhot[s][ixm1][iyp1]
+            rho12 = self.rhot[s][ix][iyp1]
+            rho22 = self.rhot[s][ixp1][iyp1]
+
+
+            # Calculate velocities at four surrounding grid points
+            mid11 = rho00 + rho10 + rho01 + rho11
+            vx11 = -2.0 * (rho10 - rho00 + rho11 - rho01) / mid11
+            vy11 = -2.0 * (rho01 - rho00 + rho11 - rho10) / mid11
+
+            mid21 = rho10 + rho20 + rho11 + rho21
+            vx21 = -2.0 * (rho20 - rho10 + rho21 - rho11) / mid21
+            vy21 = -2.0 * (rho11 - rho10 + rho21 - rho20) / mid21
+
+            mid12 = rho01 + rho11 + rho02 + rho12
+            vx12 = -2.0 * (rho11 - rho01 + rho12 - rho02) / mid12
+            vy12 = -2.0 * (rho02 - rho01 + rho12 - rho11) / mid12
+
+            mid22 = rho11 + rho21 + rho12 + rho22
+            vx22 = -2.0 * (rho21 - rho11 + rho22 - rho12) / mid22
+            vy22 = -2.0 * (rho12 - rho11 + rho22 - rho21) / mid22
+
+
+            # Calculate weights for bilinear interpolation
+            dx = rx - ix
+            dy = ry - iy
+
+            dx1m = 1.0 - dx
+            dy1m = 1.0 - dy
+
+            w11 = dx1m * dy1m
+            w21 = dx * dy1m
+            w12 = dx1m * dy
+            w22 = dx * dy
+
+            # Perform interpolation for x and y components of velocity
+            vxp = w11*vx11 + w21*vx21 + w12*vx12 + w22*vx22
+            vyp = w11*vy11 + w21*vy21 + w12*vy12 + w22*vy22
+
+            vp = (vxp, vyp)
+
+            return vp
+
+
+        def integrate_two_steps(t, h, s):
+            s0 = s
+            s1 = (s + 1) % 5
+            s2 = (s + 2) % 5
+            s3 = (s + 3) % 5
+            s4 = (s + 4) % 5
+
+            density_snapshot((t + 0.5 * h), s1)
+            density_snapshot((t + 1.0 * h), s2)
+            density_snapshot((t + 1.5 * h), s3)
+            density_snapshot((t + 2.0 * h), s4)
+
+
+            # Do three Runga-Kutta steps for each point in turn
+            esq_max = 0.0
+            drsq_max = 0.0
+            n_points = (self.xsize + 1) * (self.ysize + 1)
+
+            for p in range(0, n_points):
+                rx1 = self.grid_points_x[p]
+                ry1 = self.grid_points_y[p]
+
+                # Do big combined 2-Hour Runga-Kutta step
+                v1 = velocity(rx1, ry1, s0)
+                k1x = 2 * h * v1[0]
+                k1y = 2 * h * v1[1]
+
+                v2 = velocity((rx1 + 0.5*k1x), (ry1 + 0.5*k1y), s2)
+                k2x = 2 * h * v2[0]
+                k2y = 2 * h * v2[1]
+
+                v3 = velocity((rx1 + 0.5*k2x), (ry1 + 0.5*k1y), s3)
+                k3x = 2 * h * v3[0]
+                k3y = 2 * h * v3[1]
+
+                v4 = velocity((rx1 + 0.5*k3x), (ry1 + 0.5*k3y), s3)
+                k4x = 2 * h * v4[0]
+                k4y = 2 * h * v4[1]
+
+                dx12 = (k1x + k4x + 2.0 * (k2x + k3x)) / 6.0
+                dy12 = (k1y + k4y + 2.0 * (k2y + k3y)) / 6.0
+
+                # Do first small RK step
+                k1x = h * v1[0]
+                k1y = h * v1[1]
+
+                v2 = velocity((rx1 + 0.5 * k1x), (ry1 + 0.5 * k1y), s1)
+                k2x = h * v2[0]
+                k2y = h * v2[1]
+
+                v3 = velocity((rx1 + 0.5 * k2x), (ry1 + 0.5 * k2y), s1)
+                k3x = h * v3[0]
+                k3y = h * v3[1]
+
+                v4 = velocity((rx1 + k3x), (ry1 + k3y), s2)
+                k4x = h * v4[0]
+                k4y = h * v4[1]
+
+                dx1 = (k1x + k4x + 2.0 * (k2x + k3x)) / 6.0
+                dy1 = (k1y + k4y + 2.0 * (k2y + k3y)) / 6.0
+
+                # Do second small RK step
+                rx2 = rx1 + dx1
+                ry2 = ry1 + dy1
+
+                v1 = velocity(rx2, ry2, s2)
+                k1x = h * v1[0]
+                k1y = h * v1[1]
+
+                v2 = velocity((rx2 + 0.05*k1x), (ry2 + 0.5*k1y), s3)
+                k2x = h * v2[0]
+                k2y = h * v2[1]
+
+                v3 = velocity((rx2 + 0.5*k2x), (ry2 + 0.5*k2y), s3)
+                k3x = h * v3[0]
+                k3y = h * v3[1]
+
+                v4 = velocity((rx2 + k3x), (ry2 + k3y), s4)
+                k4x = h * v4[0]
+                k4y = h * v4[1]
+
+                dx2 = (k1x + k4x + 2.0 * (k2x + k3x)) / 6.0
+                dy2 = (k1y + k4y + 2.0 * (k2y + k3y)) / 6.0
+
+                # Calculate Squared Error
+                ex = (dx1 + dx2 - dx12) / 15
+                ey = (dy1 + dy2 - dy12) / 15
+                esq = ex*ex + ey*ey
+
+                if esq > esq_max:
+                    esq_max = esq
+
+                # Update position of vertex using more accurate result
+                # and deal with Boundary Conditions
+                dxtotal = dx1 + dx2 + ex
+                dytotal = dy1 + dy2 + ey
+
+                drsq = dxtotal*dxtotal + dytotal*dytotal
+
+                if drsq > drsq_max:
+                    drsq_max = drsq
+
+                rx3 = rx1 + dxtotal
+                ry3 = ry1 + dytotal
+
+                if rx3 < 0:
+                    rx3 = 0
+                elif rx3 > self.xsize:
+                    rx3 = self.xsize
+
+                if ry3 < 0:
+                    ry3 = 0
+                elif ry3 > self.ysize:
+                    ry3 = self.ysize
+
+                self.grid_points_x[p] = rx3
+                self.grid_points_y[p] = ry3
+
+            self.errorp = np.sqrt(esq_max)
+            self.drp = np.sqrt(drsq_max)
+
+            return s4
+
+
+        def update_running_status(t):
+            perc = int(np.round(100.0 * np.log(t / self.inith) / np.log(self.expected_time / self.inith)))
+
+            if perc > 100:
+                perc = 100
+
+            res = (700 - 350) * perc / 100
+            res += 350
+
+            print(f"Diffusion Process: {perc}% done")
+
+        def make_cartogram(blur):
+            s = 0
+            density_snapshot(0.0, s)
+
+            step = 0
+            t = 0.5 * blur * blur
+            h = self.inith
+
+            sp = None
+            self.drp = 1.0
+
+            while self.drp > 0.0:
+                sp = integrate_two_steps(t, h, s)
+
+                t += 2.0 * h
+                step += 2
+                s = sp
+
+                # Adjust time-step
+                desired_ratio = np.power((2 * self.target_error / self.errorp), 0.2)
+
+                if desired_ratio > self.max_ratio:
+                    h *= self.max_ratio
+                else:
+                    h *= desired_ratio
+
+                update_running_status(t)
+
+
+        def project_cartogram():
+            pass
+
+        density_grid, self.xsize, self.ysize = make_density_grid()
+
+        self.rhot = np.zeros((5, self.xsize, self.ysize))
+        self.fftrho = np.zeros((self.xsize, self.ysize))
+        self.fftexpt = np.zeros((self.xsize, self.ysize))
+        self.expky = np.zeros(self.ysize)
+
+        compute_initial_density(density_grid)
+
+        create_grid_of_points()
+
+        make_cartogram(0.0)
+
+        # transform = project_cartogram(cartogram)
+
+        return self.fftrho
 
     def fast_flow(self):
         pass
--- main.py
+++ main.py
@@ -145,214 +145,23 @@ if __name__ == '__main__':
 
     gdf = make_gdf(places_df, pop_df)
 
-    # squares_features, squares_keys = parse_geojson("./data/Test/square_test.geojson")
-    # squares_df = gpd.GeoDataFrame.from_features(squares_features)
-    #
-    # squares_pop = parse_pop("./data/Test/squares_pop.csv")
-    #
-    # squares_geo_pop = squares_df.merge(squares_pop, on="name")
-    # squares_geo_pop.drop(["shape"], axis=1, inplace=True)
-
-    # squares_cart = cartogram.Cartogram(squares_geo_pop, "Population", id_field="name")
-    # squares_noncon = squares_cart.non_contiguous(position="centroid", size_value=1.0)
-    # squares_dorling = squares_cart.dorling(iterations=256)
-
-    # squares_geo_pop.plot(color='w', ax=ax, zorder=0, edgecolor='0', linewidth=0.5, legend=False)
-    # squares_noncon.plot(color='r', ax=ax, edgecolor='0', linewidth=0.1, legend=False)
-    # squares_dorling.plot(color='blue', ax=ax, edgecolor='0', linewidth=0.1, legend=False)
-    # plt.savefig("squares_dorling.png", dpi=750)
-
     # gdf.plot(color='w', ax=ax, zorder=0, edgecolor='0', linewidth=0.1, legend=False)
-    # gdf.to_file('out.shp')
 
-    # londonless_geo_pop = geo_pop[geo_pop["Name"] != "LONDON"].reset_index(drop=True)
-
-    # cart = cartogram.Cartogram(geo_pop, "Population", id_field="Name")
-    # borders, islands, regions, current_region, neighbours, xrepel, yrepel, xattract, yattract, repel_dist, attract_dist = cart.dorling(iterations=1, stop=77)
-    # regions = cart.dorling(iterations=100, stop=None)
-    # diffusion = cart.diffusion()
-
-    # for i in range(1, 101):
-    #     fig, ax = plt.subplots(1, figsize=(4, 4))
-    #     ax.axis('equal')
-    #     ax.axis('off')
-    #     plt.xlim((-9.492274504549895, 2.6818561740215383))
-    #     plt.ylim((49.31499355, 61.41058145))
-    #
-    #     print(f"Running {i} iterations:\n")
-    #     regions, displacement = cart.dorling(iterations=i)
-    #     geo_pop.plot(color='w', ax=ax, alpha=0.8, zorder=0, edgecolor='0', linewidth=0.1, legend=False)
-    #     regions.plot(color='w', ax=ax, alpha=0.8, zorder=0, edgecolor='0', linewidth=0.1, legend=False)
-    #
-    #     plt.savefig(f"./dorling_out/dorling_iters_{i}.png", dpi=1200)
-    #
-    #     print("\n")
-    #     plt.cla()
-
-    # centroids = regions["geometry"].to_numpy()
-    # centroids = np.vstack(centroids)
-    # plt.plot(centroids[:, 0], centroids[:, 1], '.', color='r', markersize=0.2)
-
-    # borders_W = W.from_adjlist(borders)
-    #
-    # for k, neighs in borders_W.neighbors.items():
-    #     if k not in islands:
-    #         origin = centroids[k]
-    #         for neigh in neighs:
-    #             segment = centroids[[k, neigh]]
-    #             plt.plot(segment[:, 0], segment[:, 1], '-', linewidth=0.2)
-
-    # no_island_centroids = regions.drop(islands, axis=0, inplace=False)
-    # no_island_centroids = np.vstack(no_island_centroids["geometry"].to_numpy())
-    # plt.plot(no_island_centroids[:,0], no_island_centroids[:,1], '.', color='r', markersize=0.2)
-
-    # if islands is not None:
-    #     regions = regions.drop(islands, axis=0, inplace=False)
-    #
-    # radius_regions = gpd.GeoDataFrame(
-    #     data=regions.drop(columns=["geometry", "radius"]),
-    #     geometry=regions.apply(lambda x: x["geometry"].buffer(x["radius"]), axis=1)
-    # )
-    #
-    # neighbours_idx = neighbours.index.values.tolist()
-
+    cart = cartogram.Cartogram(gdf, "Population", id_field="Name")
+    # dorling = cart.dorling(iterations=100, stop=None)
     # non_con = cart.non_contiguous(position='centroid', size_value=1.0)
-    # dorling = cart.old_dorling(iterations=1000)
+    fftrho = cart.diffusion()
 
-    # borders = cartogram.shared_borders(geo_pop)
-    # knn_borders = cartogram.knn_borders(geo_pop)
-
-    # wq, wk, WK = borders.get_borders(geo_pop)
-    # neighbours, weights = borders.get_borders(geo_pop)
-
-    # for neighbour, weight in zip(neighbours, weights):
-    #     if len(neighbours[neighbour]) != len(weights[weight]):
-    #         print(f"Neighbour {neighbour} and Weight {weight} are mismatched")
-
-    # WQ, WK = borders.get_borders(geo_pop)
-    # W = borders.get_borders(geo_pop)
-
-    # test_geo_pop = geo_pop.copy()
-    #
-    # for island in wq.islands:
-    #     test_geo_pop.drop([island], inplace=True)
-    #
-    # test_geo_pop.plot(color='w', ax=ax, alpha=0.8, zorder=0,  edgecolor='0', linewidth=0.1, legend=False)
-
-    # if 0 in knn_borders["focal"].values:
-    #     if 3 in knn_borders[knn_borders["focal"] == 0]["neighbor"].values:
-    #         print("it is in here")
-
-    # unique_borders = knn_borders.merge(
-    #     borders,
-    #     how='left',
-    #     indicator=True
-    # )
-
-    # geo_pop.plot(color='w', ax=ax, alpha=0.8, zorder=0,  edgecolor='0', linewidth=0.1, legend=False)
-    minx, miny, maxx, maxy = gdf.total_bounds
-    gdf['density'] = gdf['Population'] / gdf.area
     # gdf.plot(color='w', ax=ax, alpha=0.8, zorder=0,  edgecolor='0', linewidth=0.1, legend=False)
-
-    # x, y = (minx, miny)
-    # geom_array = []
-    # densities = []
-
-    w = maxx - minx
-    h = maxy - miny
-
-    n_cells_x = 256
-    n_cells_y = 256
-    cell_x = w / n_cells_x
-    cell_y = h / n_cells_y
-
-    x_coords = np.arange(minx, maxx, cell_x)
-    y_coords = np.arange(miny, maxy, cell_y)
-
-    geometries = [geometry.box(x, y, x + cell_x, y + cell_y) for y in y_coords for x in x_coords]
-    fishnet = gpd.GeoDataFrame(geometry=geometries, crs=gdf.crs)
-
-    mean_density = gdf['density'].mean()
-
-    sindex = gdf.sindex
-
-    densities = []
-
-    for i, cell in fishnet.iterrows():
-        cell_geom = cell['geometry']
-        possible_matches_index = list(sindex.intersection(cell_geom.bounds))
-        possible_matches = gdf.iloc[possible_matches_index]
-        precise_matches = possible_matches[possible_matches.intersects(cell_geom)]
-        if not precise_matches.empty:
-            density = precise_matches['density'].max()
-        else:
-            density = mean_density
-        densities.append(density)
-
-    fishnet['density'] = densities
-
-    # cell_x_num = 0
-    # cell_y_num = 0
-    #
-    # while y < maxy:
-    #     while x < maxx:
-    #         print(f"Cell at (x, y): ({cell_x_num}, {cell_y_num})")
-    #         geom = geometry.Polygon([(x,y), (x, y+cell_y), (x+cell_x, y+cell_y), (x+cell_x, y), (x, y)])
-    #         geom_array.append(geom)
-    #
-    #         cell_x_num += 1
-    #
-    #         # Check if cell intersects with gdf
-    #         cell = gpd.GeoDataFrame(geometry=[geom]).set_crs('EPSG:3857')
-    #         intersection = gpd.overlay(cell, gdf, how='intersection')
-    #
-    #         if len(intersection) > 0:
-    #             print("Adding intersection density")
-    #             density = intersection['density'].iloc[0]
-    #         else:
-    #             print("Adding mean density")
-    #             density = mean_density
-    #
-    #         densities.append(density)
-    #         x += cell_x
-    #     x = minx
-    #     y += cell_y
-    #     cell_y_num += 1
-    #     cell_x_num = 0
-    #
-    # fishnet = gpd.GeoDataFrame(geom_array, columns=['geometry']).set_crs('EPSG:3857')
-    # fishnet['density'] = densities
-    fishnet.plot(facecolor='none', ax=ax, alpha=0.5, zorder=0,  edgecolor='0', linewidth=0.1, legend=False)
-
-    # joined = gpd.sjoin(fishnet, gdf, op='intersects')
-    # joined.plot(color='w', ax=ax, zorder=0, edgecolor='0', linewidth=0.1, legend=False)
-
-    # fishnet['density'] = joined['density'].fillna(mean_density)
-
-    OrRd = plt.get_cmap('OrRd')
-    trunced_OrRd = truncate_colormap(OrRd, 0.2)
-
-    fishnet.plot(column='density', cmap=trunced_OrRd, alpha=0.8, zorder=0, edgecolor='0', linewidth=0.1, legend=True)
-
-    # density = joined.groupby(['index_right']).agg({'density': 'mean'})
-
-    # non_con.plot(color='r', ax=ax, edgecolor='0', linewidth=0.1, legend=False)
-    # dorling.plot(color='blue', ax=ax, edgecolor='0', linewidth=0.1, legend=False)
-
-    # New Dorling Method Testing
-    # radius_regions.plot(color='w', ax=ax, alpha=0.8, zorder=0, edgecolor='0', linewidth=0.1, legend=False)
-    # radius_regions.loc[neighbours_idx].plot(color='b', ax=ax, alpha=0.8, zorder=0, edgecolor='0', linewidth=0.1, legend=False)
-    # radius_regions.loc[[current_region]].plot(color='r', ax=ax, alpha=0.8, zorder=0, edgecolor='0', linewidth=0.1, legend=False)
-    # regions.plot(color='w', ax=ax, alpha=0.8, zorder=0, edgecolor='0', linewidth=0.1, legend=False)
 
     # diffusion.plot(color='w', ax=ax, alpha=0.8, zorder=0,  edgecolor='0', linewidth=0.1, legend=False)
 
     # places_df.plot(ax=ax, edgecolor='0', linewidth=0.1)
 
-    # geo_pop.plot(column='Population', cmap=new_cmap, ax=ax, edgecolor='0', linewidth=0.1, legend=True)
+    # gdf.plot(column='Population', cmap=new_cmap, ax=ax, edgecolor='0', linewidth=0.1, legend=True)
     # ax.set_title('Population of LADs', fontdict={'fontsize': '15', 'fontweight': '3'})
 
     # Plot Figure
-    plt.savefig("./out/fishnet_256_density_2.png", dpi=1200)
+    # plt.savefig("./out/fishnet_256_density_2.png", dpi=1200)
 
 
